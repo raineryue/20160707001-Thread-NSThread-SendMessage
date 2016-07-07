938e09d3a3a03b9acd83e9e2700dca0249485617
Rainer <it_jinlei.yue@163.com>
2016-07-07 13:50:33 +0800

    Initial Commit

---
+++ 20160707001-Thread-NSThread-SendMessage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160707001-Thread-NSThread-SendMessage
+//
+//  Created by Rainer on 16/7/7.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 20160707001-Thread-NSThread-SendMessageTests/_0160707001_Thread_NSThread_SendMessageTests.m
@@ -0,0 +1,39 @@
+//
+//  _0160707001_Thread_NSThread_SendMessageTests.m
+//  20160707001-Thread-NSThread-SendMessageTests
+//
+//  Created by Rainer on 16/7/7.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _0160707001_Thread_NSThread_SendMessageTests : XCTestCase
+
+@end
+
+@implementation _0160707001_Thread_NSThread_SendMessageTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
